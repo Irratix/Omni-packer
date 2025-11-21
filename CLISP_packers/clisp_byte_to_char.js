8b371ec8b3f92c0f0c85a72f75affeb5fd3c8682
KG <kgscience@hotmail.com>
2025-11-20 23:46:36 -0500

    Add CLISP packers

---
+++ CLISP_packers/clisp_byte_to_char.js
@@ -0,0 +1,87 @@
+Packers["CLISP"]["code: fewest chars"] = [];
+
+Packers["CLISP"]["code: fewest chars"].push({
+    'name': "no packer",
+    'validity_check': function(code) {
+        return true;
+    },
+    'packer': function(code) {
+        return code;
+    }
+});
+
+/**** 3:1 *****/
+Packers["CLISP"]["code: fewest chars"].push({
+    'name': "3:1",
+    'limitations': "Must contain only ASCII characters above code 31.",
+    'validity_check': function(code) {
+        code = new TextEncoder().encode(code);
+        for (const c of code) 
+            if (c < 32 || c > 127)
+                return false;
+        return true;
+    },
+	'tips': [
+		"Remove newlines in format strings using `~%`.",
+        "The top level must be a single form.",
+    ],
+    'packer': function(code) {
+        let compressed = "";
+		code += "#.";
+        code += ' '.repeat(code.length * 2 % 3);
+		code = new TextEncoder().encode(code).reverse();
+        for (let i = 0; i < len; i += 3) {
+            let s = 0, p = 99 * 100 * 101;
+            // 99
+            let q = 100 * 101;
+            s += q * (code[i] - 32) * 50;
+            // 100
+            q = 99 * 101;
+            s += q * (code[i + 1] - 32) * 99;
+            // 101
+            q = 99 * 100;
+            s += q * (code[i + 2] - 32) * 51;
+            // code point reached
+            compressed += String.fromCodePoint(s % p);
+        }
+        return `(read-from-string(map'string'int-char(doseq(c"${compressed}"/)(dotimes'3(push(+(mod(char-int c)(+ .'97))32)/)))))`;
+    }
+});
+
+/**** 3:1 (uppercase) *****/
+Packers["CLISP"]["code: fewest chars"].push({
+    'name': "3:1 (uppercase)",
+    'limitations': "Must contain only ASCII characters below code 123.",
+    'validity_check': function(code) {
+        code = new TextEncoder().encode(code);
+        for (const c of code) 
+            if (c > 122)
+                return false;
+        return true;
+    },
+	'tips': [
+		"Do not use if you have a string literal with necessarily lowercase content.",
+        "The top level must be a single form.",
+    ],
+    'packer': function(code) {
+        let compressed = "";
+		code += "#.";
+        code += ' '.repeat(code.length * 2 % 3);
+		code = new TextEncoder().encode(code.toUpperCase()).reverse();
+        for (let i = 0; i < len; i += 3) {
+            let s = 0, p = 99 * 100 * 101;
+            // 99
+            let q = 100 * 101;
+            s += q * (code[i] - 32) * 50;
+            // 100
+            q = 99 * 101;
+            s += q * (code[i + 1] - 32) * 99;
+            // 101
+            q = 99 * 100;
+            s += q * (code[i + 2] - 32) * 51;
+            // code point reached
+            compressed += String.fromCodePoint(s % p);
+        }
+        return `(read-from-string(map'string'int-char(doseq(c"${compressed}"/)(dotimes'3(push(mod(char-int c)(+ .'97))/)))))`;
+    }
+});
